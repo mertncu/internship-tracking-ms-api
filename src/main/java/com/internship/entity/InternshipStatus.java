20c7643486b9a0b887c18c805b6d8aa78e004413
mertuncu <mertncu@gmail.com>
2025-03-24 15:38:42 +0300

    Add Intership Status

---
+++ src/main/java/com/internship/entity/InternshipStatus.java
@@ -0,0 +1,12 @@
+package com.internship.entity;
+
+public enum InternshipStatus {
+    PENDING_ADVISOR_APPROVAL,    
+    PENDING_COORDINATOR_APPROVAL,  
+    APPROVED,                     
+    REJECTED,                     
+    IN_PROGRESS,                  
+    COMPLETED,                    
+    DOCUMENT_WAITING,             
+    GRADED                        
+} 
