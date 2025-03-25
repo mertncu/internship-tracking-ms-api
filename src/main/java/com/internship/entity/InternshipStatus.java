50158a2250e82b5095237615d2c212967d068f93
mertuncu <mertncu@gmail.com>
2025-03-25 21:19:33 +0300

    Add InternshipType and update InternshipStatus enums

--- src/main/java/com/internship/entity/InternshipStatus.java
+++ src/main/java/com/internship/entity/InternshipStatus.java
@@ -1,12 +1,16 @@
 package com.internship.entity;
 
 public enum InternshipStatus {
-    PENDING_ADVISOR_APPROVAL,    
-    PENDING_COORDINATOR_APPROVAL,  
-    APPROVED,                     
-    REJECTED,                     
-    IN_PROGRESS,                  
-    COMPLETED,                    
-    DOCUMENT_WAITING,             
-    GRADED                        
+    PENDING,                // İlk başvuru durumu
+    ADVISOR_APPROVED,       // Danışman onayladı
+    ADVISOR_REJECTED,       // Danışman reddetti
+    REVISION_REQUESTED,     // Düzeltme istendi
+    DEPARTMENT_APPROVED,    // Bölüm koordinatörü onayladı
+    DEPARTMENT_REJECTED,    // Bölüm koordinatörü reddetti
+    UNIVERSITY_APPROVED,    // Üniversite koordinatörü onayladı
+    UNIVERSITY_REJECTED,    // Üniversite koordinatörü reddetti
+    CANCELLED,             // İptal edildi
+    EXTENDED,              // Uzatıldı
+    COMPLETED,             // Tamamlandı
+    EVALUATED              // Değerlendirildi
 } 
---
+++ src/main/java/com/internship/entity/InternshipType.java
@@ -0,0 +1,7 @@
+package com.internship.entity;
+
+public enum InternshipType {
+    COMPULSORY,    // Zorunlu staj
+    VOLUNTARY,     // Gönüllü staj
+    ONLINE         // Online staj
+} 
